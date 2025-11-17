509c102044ba031c5a13f0ad3a1e038c73702096
Liam Nevils <lnevils98@gmail.com>
2025-11-17 15:21:19 -0500

    first commit

---
+++ llama_test.py
@@ -0,0 +1,14 @@
+from ollama import Client
+
+client = Client(
+    host='http://localhost:11434',
+)
+
+response = client.chat(
+    model='llama3.2',
+    messages=[
+        {'role': 'user', 'content': 'Explain pods vs deployments.'},
+    ],
+)
+
+print(response)
